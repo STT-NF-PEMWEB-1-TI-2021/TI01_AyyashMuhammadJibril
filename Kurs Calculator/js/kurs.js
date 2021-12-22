eaad1af81a5f5e0690ce7de4179f500fa1f629e9
Mirezka <39770337+Mirezka@users.noreply.github.com>
2021-12-22 16:31:47 +0700

    Delete Kurs Calculator directory

--- Kurs Calculator/js/kurs.js
+++
@@ -1,6 +0,0 @@
-function inputDollar() {
-    const usd = 14650;
-    let dollarInput = Number(document.getElementById("input").value);
-    let hasil = dollarInput * usd;
-    document.getElementById("result").value = hasil;
-}
