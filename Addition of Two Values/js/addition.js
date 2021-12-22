195b96693bb43464c82ec24c5b38d59ba84d3b44
Mirezka <39770337+Mirezka@users.noreply.github.com>
2021-12-22 16:31:25 +0700

    Delete Addition of Two Values directory

--- Addition of Two Values/js/addition.js
+++
@@ -1,6 +0,0 @@
-function kalkulasi(){
-    let val1 = Number(document.getElementById("bil1").value);
-    let val2 = Number(document.getElementById("bil2").value);
-    let sum = val1 + val2
-    document.getElementById("hasil").value = sum;
-}
